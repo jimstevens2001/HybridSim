c2e81859865179a868bf6451486d4f3a6caab6fc
Jim Stevens <jims@cs.umd.edu>
2011-03-08 03:07:21 -0500

    Removed OpenCode.sh

--- OpenCode.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-emacs TraceBasedSim.cpp HybridSystem.cpp --eval '(delete-other-windows)'&
-
-emacs TraceBasedSim.h HybridSystem.h HybridSim.h FDSim.h config.h CallbackHybrid.h --eval '(delete-other-windows)'&
-
-echo opening files
