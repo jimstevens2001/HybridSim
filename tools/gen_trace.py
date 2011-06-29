ae2ec8e9ba86b70b5ea04461e7a260a2b8835b61
Jim Stevens <jims@cs.umd.edu>
2011-06-28 20:57:35 -0400

    updated gen_trace.py to take multiple traces

--- tools/gen_trace.py
+++ tools/gen_trace.py
@@ -1,29 +1,35 @@
 import sys
 import struct
 
-with open(sys.argv[1], 'r') as f:
-    length = 1
-    byte = f.read(1)
-    while ord(byte) != 0:
-        byte = f.read(1)
-        length += 1
+base = sys.argv[1]
+num_input = sys.argv[2]
 
-    cycle = 0
+files = [base+str(i) for i in range(num_input)]
 
-    (byte,) = struct.unpack('<L', f.read(4))
-    length = byte - length - 4
-    while length:
-        (flags,) = struct.unpack('B', f.read(1))
-        (threadid,) = struct.unpack('B', f.read(1))
-        (size,) = struct.unpack('<H', f.read(2))
-        (vaddr,) = struct.unpack('<Q', f.read(8))
-        (paddr,) = struct.unpack('<Q', f.read(8))
-        #print hex(flags), hex(threadid), hex(size), hex(vaddr), hex(paddr)
-        length -= 20
+cycle = 0
 
-        writebit = flags
-        print str(cycle) + '\t\t' + str(writebit) + '\t\t' + str(paddr)
-        cycle += 1000 # space out 1000 cycles to keep queue from jamming up.
+for filename in files:
+	with open(filename, 'r') as f:
+		length = 1
+		byte = f.read(1)
+		while ord(byte) != 0:
+			byte = f.read(1)
+			length += 1
+
+		(byte,) = struct.unpack('<L', f.read(4))
+		length = byte - length - 4
+		while length:
+			(flags,) = struct.unpack('B', f.read(1))
+			(threadid,) = struct.unpack('B', f.read(1))
+			(size,) = struct.unpack('<H', f.read(2))
+			(vaddr,) = struct.unpack('<Q', f.read(8))
+			(paddr,) = struct.unpack('<Q', f.read(8))
+			#print hex(flags), hex(threadid), hex(size), hex(vaddr), hex(paddr)
+			length -= 20
+
+			writebit = flags
+			print str(cycle) + '\t\t' + str(writebit) + '\t\t' + str(paddr)
+			cycle += 1000 # space out 1000 cycles to keep queue from jamming up.
 
 #        if cycle > 100000:
 #            break
