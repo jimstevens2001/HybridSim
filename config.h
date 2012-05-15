bb90fd37cd52f5a46e31900c55343e36c8e4d178
Jim Stevens <jims@cs.umd.edu>
2012-05-15 03:39:56 -0400

    Set OVERRIDE_DRAM_SIZE to 0 by default.

--- config.h
+++ config.h
@@ -87,7 +87,7 @@ const uint64_t FOURGB = 4294967296; // 1024^3 * 4
 // OVERRIDE_DRAM_SIZE is used to make the dram_size parameter passed to DRAM different than the computed
 // size needed by HybridSim (CACHE_PAGES * PAGE_SIZE) >> 20. 
 // If it is 0, it is ignored.
-#define OVERRIDE_DRAM_SIZE 1024
+#define OVERRIDE_DRAM_SIZE 0
 
 // C standard library and C++ STL includes.
 #include <iostream>
